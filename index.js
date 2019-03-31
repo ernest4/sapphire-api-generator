20a8d25f344eb3a8eabbea10294ccdc49d2600b8
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2019-03-31 22:08:19 +0100

    wip

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ program
   .option("--no-git", "don't add a git repo")
   .option("--no-intro", "don't add the _intro.txt files which explain the directories")
   .option("-H, --heroku", "add a Heroku Procfile for deploying to Heroku")
-  // .option("--test <test>", "that's just for me don't mind that ;)")
   .action((app_name, options) => {
-    console.log(`app name: ${app_name}, test: ${options.test}`);
-    if (options.heroku) console.log("yay heroku!");
-    if (!options.git) console.log("no git!");
+    // console.log(`app name: ${app_name}, test: ${options.test}`);
+    // if (options.heroku) console.log("yay heroku!");
+    // if (!options.git) console.log("no git!");
+
+    console.log(
+      `
+      initializing the api for ${app_name}:
+
+        ${actions(options)}
+      `
+    );
   });
 
 program
@@ -29,26 +36,45 @@ program
     const version = `/api/v${options.api_version || 1}`;
 
     console.log(
-    `
-    generating asset "${asset}":
+      `
+      generating asset "${asset}":
 
-    routes:     ${version}/routes/${asset}.routes.js
-    controller: ${version}/controllers/${asset}.controller.js
-    model:      ${version}/models/${asset}.model.js
-    service:    ${version}/services/${asset}.service.js
+      routes:     ${version}/routes/${asset}.routes.js
+      controller: ${version}/controllers/${asset}.controller.js
+      model:      ${version}/models/${asset}.model.js
+      service:    ${version}/services/${asset}.service.js
 
-    Endpoints:
-      GET:    ${version}/${asset}
-      POST:   ${version}/${asset}
+      Endpoints:
+        GET:    ${version}/${asset}
+        POST:   ${version}/${asset}
 
-      GET:    ${version}/${asset}/:${asset}Id
-      PUT:    ${version}/${asset}/:${asset}Id
-      DELETE: ${version}/${asset}/:${asset}Id
+        GET:    ${version}/${asset}/:${asset}Id
+        PUT:    ${version}/${asset}/:${asset}Id
+        DELETE: ${version}/${asset}/:${asset}Id
 
-    `
+      `
     );
   });
 
-program.command("seed user 100").alias("s");
+program
+  .command("seed <asset>")
+  .alias("s")
+  .description("generate dummy data for chosen asset")
+  .option("-c, --count <count>", "specify the number of instances of the asset")
+  .action((asset, options) => {
+    const count = options.count || 10;
+
+    console.log(
+      `
+      seeding asset "${asset}":
+      count: ${count}
+      
+      `
+    );
+  });
 
 program.parse(process.argv);
+
+function actions(options) {
+  return "initializing...";
+}
