0d671d9374715f980ae0e1827430ef9412353d27
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2019-03-31 20:18:58 +0100

    added some commands

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log("testy");
+
+const program = require("commander");
+
+const VERSION = "0.1.0"; // TODO: read this from package.json
+
+program
+  .arguments("<app_name>")
+  .version(`v${VERSION}`)
+  .option("--no-git", "don't add a git repo")
+  .option("-H, --heroku", "add a Heroku Procfile")
+  .option("--test <test>", "that's just for me don't mind that ;)")
+  .action(app_name => {
+    console.log(`app name: ${app_name}, test: ${program.test}`);
+    if (program.heroku) console.log("yay heroku!");
+    if (!program.git) console.log("no git!");
+  })
+  .parse(process.argv);
