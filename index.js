c8259dbab6483d736da5cdd7c677e7c88114406a
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2019-03-31 20:36:58 +0100

    beginnings of command line

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const program = require("commander");
 
 const VERSION = "0.1.0"; // TODO: read this from package.json
 
+program.version(`v${VERSION}`);
+
 program
-  .arguments("<app_name>")
-  .version(`v${VERSION}`)
+  .command('init <app_name>')
+  .description('initialize the base structure of your application')
   .option("--no-git", "don't add a git repo")
-  .option("-H, --heroku", "add a Heroku Procfile")
-  .option("--test <test>", "that's just for me don't mind that ;)")
-  .action(app_name => {
-    console.log(`app name: ${app_name}, test: ${program.test}`);
-    if (program.heroku) console.log("yay heroku!");
-    if (!program.git) console.log("no git!");
-  })
-  .parse(process.argv);
+  .option("-H, --heroku", "add a Heroku Procfile for deploying to Heroku")
+  // .option("--test <test>", "that's just for me don't mind that ;)")
+  .action((app_name, options) => {
+    console.log(`app name: ${app_name}, test: ${options.test}`);
+    if (options.heroku) console.log("yay heroku!");
+    if (!options.git) console.log("no git!");
+  });
+  
+program.parse(process.argv);
