a284db840c2959c8da27cfd73b6fc9f7b8cb24a6
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2019-04-02 08:14:26 +0100

    adding the outline of future features, updateing assets and generating complete examples

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ program
 
     let initGenerator = init(app_name, options);
     for (let iteration = initGenerator.next(); !iteration.done; iteration = initGenerator.next()) {
-      if(iteration.value) console.log(`       ${iteration.value}`);
+      if (iteration.value) console.log(`       ${iteration.value}`);
     }
   });
 
@@ -82,6 +82,61 @@ program
     console.log(seed(asset, options));
   });
 
+// // TODO: implement in the future
+// program
+//   .command("update <asset> [args...]")
+//   .alias("u")
+//   .description(`change the asset's model or add relationships to other models:
+  
+//   RELATIONSHIP: [one to many]: asset1 has many asset2
+
+//     EXAMPLE 1: one author has many books.
+
+//     $ sapphire update author has many book
+
+//   RELATIONSHIP: [many to many]: asset1 many to many asset2
+
+//     EXAMPLE 2: An editor has worked on many articles and an article can have many editors.
+    
+//     $ sapphire update editor many to many article
+
+//     EXAMPLE 3: alternatively, you may define relationships one by one.
+
+//     $ sapphire update editor has many article
+//     $ sapphire update article has many editor
+//   `)
+//   .action((asset, args) => {
+//     // TODO: implement in the future
+//     console.log(asset);
+//     console.log(args);
+//   });
+
+// // TODO: implement in the future
+// program
+//   .command("example <project>")
+//   .alias("e")
+//   .option("-a, --auth", "add authorization for the example")
+//   .description(`Bootstrap an example API with at least two related models:
+
+//   RELATIONSHIP: [one to many]: asset1 has many asset2
+
+//     EXAMPLE 1: Will create an api for tasks and todos, where each task has a list of todos.
+
+//     $ sapphire example todos
+
+//   RELATIONSHIP: [many to many]: asset1 many to many asset2
+
+//     EXAMPLE 2: Will create an api for customers and items, where each customer has many
+//               items in their basket, but an item can belong to many customers too.
+
+//     $ sapphire example shoppers
+//   `)
+//   .action((project, options) => {
+//     // TODO: implement in the future
+//     console.log(project);
+//     console.log(options);
+//   });
+
 program.parse(process.argv);
 
 function* init(app_name, options) {
@@ -91,7 +146,7 @@ function* init(app_name, options) {
 
   try {
     yield `creating directories:
-    `
+    `;
     for (const dir of dirs) yield dc.apiDirCreator(app_name, version, dir);
     yield dc.dirCreator(`${app_name}/api/shared/middleware`);
     yield dc.dirCreator(`${app_name}/_utils`);
@@ -130,4 +185,8 @@ function* generate(asset, options) {
 
 function* seed(asset, options) {
   return "seeding...";
+}
+
+function* update(asset, options) {
+  return "updating...";
 }
